31879752a19bd80e174a4b826e126a2cc9429c31
StAl15 <stihinaleksander@yandex.ru>
2024-04-07 17:38:25 +0500

    test my build

---
+++ src/test/java/robots/model/log/LogLevelDiffblueTest.java
@@ -0,0 +1,21 @@
+package robots.model.log;
+
+import org.junit.jupiter.api.Test;
+
+class LogLevelDiffblueTest {
+    /**
+     * Method under test: {@link LogLevel#level()}
+     */
+    @Test
+    void testLevel() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        LogLevel logLevel = LogLevel.Trace;
+
+        // Act
+        int actualLevelResult = logLevel.level();
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+}
